beaacb8f3ffbf9f0e240d82a95aff2e3d2ced3c4
Martyzz1 <martyzz1@gmail.com>
2012-10-08 19:02:55 +0100

    Add the celery checker

---
+++ heroku_proc_scalar/redis_celerycheck.py
@@ -0,0 +1,20 @@
+import sys
+from urlparse import urlparse, uses_netloc
+import redis
+from django.conf import settings
+uses_netloc.append('redis')
+
+redis_queue_url = urlparse(settings.BROKER_URL)
+queue = redis.StrictRedis(
+      host=redis_queue_url.hostname,
+      port=int(redis_queue_url.port),
+      db=int(redis_queue_url.path[1:]),
+      password=redis_queue_url.password
+    )
+
+DISABLE_CELERY = queue.get('DISABLE_CELERY')
+if DISABLE_CELERY and DISABLE_CELERY != '0':
+    print "Celery disabled for %s exiting..." % DISABLE_CELERY
+    sys.exit(0)
+else:
+    print "Celery enabled running..."
