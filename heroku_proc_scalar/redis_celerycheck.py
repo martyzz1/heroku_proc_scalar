36b5a37578a1f7a47e64d36b9a43297290723011
Martyzz1 <martyzz1@gmail.com>
2012-10-08 23:19:32 +0100

    Experimental

--- heroku_proc_scalar/redis_celerycheck.py
+++ heroku_proc_scalar/redis_celerycheck.py
@@ -2,6 +2,7 @@ import sys
 from urlparse import urlparse, uses_netloc
 import redis
 from django.conf import settings
+from celery.apps.worker import Worker
 uses_netloc.append('redis')
 
 redis_queue_url = urlparse(settings.BROKER_URL)
@@ -12,6 +13,8 @@ queue = redis.StrictRedis(
       password=redis_queue_url.password
     )
 
+w = Worker()
+print "worker hostname = %s" % w.hostname
 DISABLE_CELERY = queue.get('DISABLE_CELERY')
 if DISABLE_CELERY and DISABLE_CELERY != '0':
     print "Celery disabled for %s exiting..." % DISABLE_CELERY
