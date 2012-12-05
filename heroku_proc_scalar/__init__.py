346913a30d1a1093ef6fe3d9f2e0b96a547da2cb
Martyzz1 <martyzz1@gmail.com>
2012-12-05 16:50:15 +0000

    Minor tweak

--- heroku_proc_scalar/__init__.py
+++ heroku_proc_scalar/__init__.py
@@ -14,6 +14,7 @@ def get_queue_maps():
     queuemaps = {}
     e = re.compile("(\w+):.+(--queues|-Q)\s+(\w[\w,\s]*)")
     c = re.compile("(\w+):.+bin\/celery_shutdown.py")
+    print "Opening procfile %x\n" % PROCFILE
     f = open(PROCFILE)
     lines = f.readlines()
     f.close()
