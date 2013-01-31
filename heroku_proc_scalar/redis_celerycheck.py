3e6b981feda9532305f1627797d972790157d130
Martyzz1 <martyzz1@gmail.com>
2013-01-31 16:53:49 +0000

    Update to use the config, not redis

--- heroku_proc_scalar/redis_celerycheck.py
+++ heroku_proc_scalar/redis_celerycheck.py
@@ -1,28 +1,23 @@
 import sys
-from urlparse import urlparse, uses_netloc
-import redis
-from pprint import pprint
-from django.conf import settings
-from celery.app import current_app as current_celery
-from celery.bin.celeryd import WorkerCommand
 import os
+import heroku
+from pprint import pprint
 
-uses_netloc.append('redis')
-
-CELERY_HOSTNAME = os.environ.get('CELERY_HOSTNAME', False)
+CELERY_HOSTNAME = os.environ.get('CELERY_HOSTNAME', None)
 assert(CELERY_HOSTNAME)
 
-redis_queue_url = urlparse(settings.PROC_SCALAR_DB)
-queue = redis.StrictRedis(
-      host=redis_queue_url.hostname,
-      port=int(redis_queue_url.port),
-      db=int(redis_queue_url.path[1:]),
-      password=redis_queue_url.password
-    )
+HEROKU_API_KEY = os.environ.get('HEROKU_API_KEY', None)
+HEROKU_APPNAME = os.environ.get('HEROKU_APPNAME', None)
 
-print "Using BROKER_URL of %s" % settings.PROC_SCALAR_DB
+heroku_conn = heroku.from_key(HEROKU_API_KEY)
+heroku_app = heroku_conn.apps[HEROKU_APPNAME]
 
-DISABLE_CELERY = queue.get('DISABLE_CELERY_%s' % CELERY_HOSTNAME)
+
+key = 'DISABLE_CELERY_%s' % CELERY_HOSTNAME
+DISABLE_CELERY = heroku_app.config[key]
+
+pprint(heroku_app.config)
+
 if DISABLE_CELERY and DISABLE_CELERY != '0':
     print "Celery disabled for %s exiting..." % DISABLE_CELERY
     sys.exit(0)
