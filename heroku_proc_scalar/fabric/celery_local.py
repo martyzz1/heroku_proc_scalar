aff84f624ad9b73db8fb09c663c1f582ce1c7cf4
Martyzz1 <martyzz1@gmail.com>
2013-06-27 12:11:33 +0100

    Fixes for name changes

--- heroku_proc_scalar/fabric/celery_local.py
+++ heroku_proc_scalar/fabric/celery_local.py
@@ -3,25 +3,25 @@ from fabric.api import task, local, env
 
 @task
 def lock_remote_celery_for_deployment():
-    cmd = "heroku run fab lock_celery_for_deployment --app %s" % env.app_name
+    cmd = "heroku run fab celery.lock_celery_for_deployment --app %s" % env.app_name
     local(cmd)
 
 
 @task
 def unlock_remote_celery_after_deployment():
-    cmd = "heroku run fab unlock_celery_after_deployment --app %s" % env.app_name
+    cmd = "heroku run fab celery.unlock_celery_after_deployment --app %s" % env.app_name
     local(cmd)
 
 
 @task
 def shutdown_remote_celery():
-    cmd = "heroku run fab shutdown_celery_process_for_deployment --app %s" % env.app_name
+    cmd = "heroku run fab celery.shutdown_celery_process_for_deployment --app %s" % env.app_name
     local(cmd)
 
 
 @task
 def get_remote_running_processes():
-    cmd = "heroku run fab print_running_processes --app %s" % env.app_name
+    cmd = "heroku run fab celery.print_running_processes --app %s" % env.app_name
     result = local(cmd, capture=True)
     if result.failed:
         print result.stderr
@@ -43,5 +43,5 @@ def get_remote_running_processes():
 
 @task
 def restart_remote_processes(proclist_str):
-    cmd = "heroku run fab restart_processes_after_deployment:%s --app %s" % (proclist_str, env.app_name)
+    cmd = "heroku run fab celery.restart_processes_after_deployment:%s --app %s" % (proclist_str, env.app_name)
     local(cmd)
