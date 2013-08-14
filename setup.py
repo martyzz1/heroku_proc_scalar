a23a8b4dd462cfce211b04b8b23f4cd0dab1af93
Robin Edwards <robin.ge@gmail.com>
2013-08-14 14:11:53 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='heroku_proc_scalar',
-        version='1.0.1',
-        description="A suite of tools (mainly for Celery/Redis|IronMQ Backend) designed to allow safe shuting down of heroku procs that need to ibe terminated gracefull with SIGTERM - that likely take longer than heroku's 10 second rule. use in conjunction with the heroku_proc_scala_app",
-        author='Martin Moss',
-        author_email='martin_moss@btinternet.com',
-        url='http://github.com/martyzz1/heroku_proc_scalar',
-        license='MIT',
-        packages=find_packages(),
-        keywords='celery, djcelery, heroku, autoscaling, redis, iron_mq',
-        zip_safe=True,
-        install_requires=['Fabric==1.4.3', 'heroku==0.1.2', 'Django==1.4.5', 'iron-mq==0.4']
-        )
+    name='heroku_proc_scalar',
+    version='1.0.2',
+    description="A suite of tools (mainly for Celery/Redis|IronMQ Backend) designed to allow safe shuting down of heroku procs that need to ibe terminated gracefull with SIGTERM - that likely take longer than heroku's 10 second rule. use in conjunction with the heroku_proc_scala_app",
+    author='Martin Moss',
+    author_email='martin_moss@btinternet.com',
+    url='http://github.com/martyzz1/heroku_proc_scalar',
+    license='MIT',
+    packages=find_packages(),
+    keywords='celery, djcelery, heroku, autoscaling, redis, iron_mq',
+    zip_safe=True,
+    install_requires=['Fabric==1.4.3', 'heroku==0.1.2', 'Django==1.4.5']
+)
