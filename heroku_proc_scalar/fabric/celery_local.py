d3eeb8d25904503e85a74c81ae0411d5ceec6ea7
Martyzz1 <martyzz1@gmail.com>
2013-09-19 10:17:40 +0100

    oopos

--- heroku_proc_scalar/fabric/celery_local.py
+++ heroku_proc_scalar/fabric/celery_local.py
@@ -3,27 +3,27 @@ from fabric.api import task, local, env
 
 @task
 def lock_remote_for_deployment():
-    cmd = "heroku run fab celery.lock_for_deployment"
+    cmd = "fab celery.lock_for_deployment"
     env.app.run_command(cmd)
 
 
 @task
 def unlock_remote_after_deployment():
-    cmd = "heroku run fab celery.unlock_after_deployment"
+    cmd = "fab celery.unlock_after_deployment"
     env.app.run_command(cmd)
     local(cmd)
 
 
 @task
 def shutdown_remote():
-    cmd = "heroku run fab celery.shutdown_process_for_deployment"
+    cmd = "fab celery.shutdown_process_for_deployment"
     env.app.run_command(cmd)
     local(cmd)
 
 
 @task
 def get_remote_running_processes():
-    cmd = "heroku run fab celery.print_running_processes"
+    cmd = "fab celery.print_running_processes"
     output, dyno = env.app.run_command(cmd)
     result = local(cmd, capture=True)
 
@@ -42,5 +42,5 @@ def get_remote_running_processes():
 
 @task
 def restart_remote_processes(proclist_str):
-    cmd = "heroku run fab celery.restart_processes_after_deployment:%s" % proclist_str
+    cmd = "fab celery.restart_processes_after_deployment:%s" % proclist_str
     env.app.run_command(cmd)
