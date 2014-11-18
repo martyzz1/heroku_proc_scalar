04ec4e525b81beaf17198b7f1c66307981ac6402
Martyzz1 <martin@sharehood.co.uk>
2014-11-18 12:30:12 +0000

    naming changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='heroku_proc_scalar',
+    name='heroku-proc-scalar',
     version='2.0.5',
     description="A suite of tools (mainly for Celery/Redis|IronMQ Backend) designed to allow safe shuting down of heroku procs that need to ibe terminated gracefull with SIGTERM - that likely take longer than heroku's 10 second rule. use in conjunction with the heroku_proc_scala_app",
     author='Martin Moss',
