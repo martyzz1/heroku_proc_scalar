8f29b235bbd3a2554a28e4c79ed858b986b3cc46
Martyzz1 <martin@sharehood.co.uk>
2014-11-18 12:20:36 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='heroku_proc_scalar',
-    version='2.0.4',
+    version='2.0.5',
     description="A suite of tools (mainly for Celery/Redis|IronMQ Backend) designed to allow safe shuting down of heroku procs that need to ibe terminated gracefull with SIGTERM - that likely take longer than heroku's 10 second rule. use in conjunction with the heroku_proc_scala_app",
     author='Martin Moss',
     author_email='martin_moss@btinternet.com',
