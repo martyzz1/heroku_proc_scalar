d168805b22b10195e39f69a0a99320222231c419
Martyzz1 <martin@sharehood.co.uk>
2014-11-26 11:25:16 +0000

    Move to heroku3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='heroku-proc-scalar',
-    version='2.0.5',
+    version='2.0.6',
     description="A suite of tools (mainly for Celery/Redis|IronMQ Backend) designed to allow safe shuting down of heroku procs that need to ibe terminated gracefull with SIGTERM - that likely take longer than heroku's 10 second rule. use in conjunction with the heroku_proc_scala_app",
     author='Martin Moss',
     author_email='martin_moss@btinternet.com',
@@ -14,10 +14,7 @@ setup(
     install_requires=[
         'Fabric==1.4.3',
         'celery>=3.1.0',
-        'heroku-dev>=3.1.0',
+        'heroku3>=3.1.2',
         'redis==2.7.5',
     ],
-    dependency_links=[
-        'https://github.com/martyzz1/heroku.py/zipball/988176ed#egg=heroku-dev-3.1.0'
-    ]
 )
